6b4550c984591fd9c2d2b883bea8cc8ad755e497
Phan Trang <phantrang3006@gmail.com>
2018-03-31 13:42:40 +0700

    Update code

--- src/model/AttackPlayer.java
+++ src/model/AttackPlayer.java
@@ -6,5 +6,7 @@ public abstract class AttackPlayer extends Player {
     private int isStuned;
     private int isPowered;
 
-    
+    abstract public void attack(AttackPlayer attackPlayer);
+    abstract public void guard(AttackPlayer attackPlayer);
+    abstract public void power();
 }
---
+++ src/model/KnightPlayer.java
@@ -0,0 +1,19 @@
+package model;
+
+public class KnightPlayer extends AttackPlayer {
+
+    @Override
+    public void attack(AttackPlayer attackPlayer) {
+
+    }
+
+    @Override
+    public void guard(AttackPlayer attackPlayer) {
+
+    }
+
+    @Override
+    public void power() {
+
+    }
+}
