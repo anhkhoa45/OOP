88861fe3a16998775dd355c9cbb0c3ca1c0cbd5b
anhkhoahapo <khoand@haposoft.com>
2018-04-07 14:12:39 +0700

    Fix login

--- vue_app/src/components/Login.vue
+++ vue_app/src/components/Login.vue
@@ -13,7 +13,7 @@
           <label for="passwordInp">Password</label>
           <input id="passwordInp" type="password" name="password" class="form-control">
         </div>
-        <button type="button" class="btn btn-primary" onclick="login()">Login</button>
+        <button type="button" class="btn btn-primary" @click="login">Login</button>
       </form>
     </div>
   </div>
@@ -28,8 +28,8 @@
         let form = document.forms["loginForm"];
         post('/api/authentication/login', {'email': form.email.value, 'password': form.password.value})
           .then(({data}) => {
-            localStorage.setItem('token', data.token);
-            alert('login success');
+            this.$store.commit('saveToken', data.token);
+            this.$router.push({name: 'welcomeScreen'});
           })
           .catch(() => {
             document.getElementById('error').classList.add('active');
